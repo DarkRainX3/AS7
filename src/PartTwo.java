d50d4df3acc9ef4955e12150505f43652d6cfef7
Waley <44916399+DarkRainX3@users.noreply.github.com>
2018-11-14 00:06:54 -0700

    a

--- src/PartTwo.java
+++ src/PartTwo.java
@@ -1,17 +1,15 @@
-
 public class PartTwo {
 	public int binarySearch(int [] arr, int key) {
-		int lo=0, mid,hi = arr.length-1;// 1 + 0 + 3
-		while (lo<=hi) {//n
-			mid = (lo+hi)/2;//3*(n-1)
-			if (key<arr[mid])//2*(n-1)
-				hi=mid-1;//2
-			else if (arr[mid]<key)//2*(n-1)
-				lo=mid+1;//2
+		int lo = 0, mid, hi = arr.length - 1; // 1 + 0 + 3
+		while (lo <= hi) {// log n 
+			mid = (lo + hi)/2;
+			if (key < arr[mid])
+				hi = mid - 1;
+			else if (arr[mid] < key)
+				lo = mid + 1;
 			else
-				return mid;//1*n
+				return mid;
 		}
-		return-1;//1
+		return -1;
 	}
-
 }
